e21917f24cf8aab93391fef2e365a64ca04591da
Joke de Buhr <joke@seiken.de>
2021-01-16 13:56:44 +0100

    additional tests

--- examples/spock13/src/test/groovy/io/github/joke/spockmockable/PersonTest.groovy
+++ examples/spock13/src/test/groovy/io/github/joke/spockmockable/PersonTest.groovy
@@ -16,54 +16,54 @@ class PersonTest extends Specification {
         mockUtil.isMock person
     }
 
-//    def 'final from subclass is removed'() {
-//        setup:
-//        Person.Address address = Mock()
-//
-//        expect:
-//        mockUtil.isMock address
-//    }
-//
-//    def 'final from method is removed'() {
-//        setup:
-//        Person person = Mock()
-//
-//        when:
-//        def res = person.firstName
-//
-//        then:
-//        1 * person.firstName >> 'Dorothy'
-//
-//        expect:
-//        res == 'Dorothy'
-//    }
-//
-//    def 'privat on method is now protected'() {
-//        setup:
-//        Person person = Mock()
-//
-//        when:
-//        def res = person.lastName
-//
-//        then:
-//        1 * person.lastName >> 'Gale'
-//
-//        expect:
-//        res == 'Gale'
-//    }
-//
-//    def 'final is removed and privat on method is now protected'() {
-//        setup:
-//        Person person = Mock()
-//
-//        when:
-//        def res = person.address.street
-//
-//        then:
-//        1 * person.address >> new Person.Address('Yellow Brick Road')
-//
-//        expect:
-//        res == 'Yellow Brick Road'
-//    }
+    def 'final from subclass is removed'() {
+        setup:
+        Person.Address address = Mock()
+
+        expect:
+        mockUtil.isMock address
+    }
+
+    def 'final from method is removed'() {
+        setup:
+        Person person = Mock()
+
+        when:
+        def res = person.firstName
+
+        then:
+        1 * person.firstName >> 'Dorothy'
+
+        expect:
+        res == 'Dorothy'
+    }
+
+    def 'privat on method is now protected'() {
+        setup:
+        Person person = Mock()
+
+        when:
+        def res = person.lastName
+
+        then:
+        1 * person.lastName >> 'Gale'
+
+        expect:
+        res == 'Gale'
+    }
+
+    def 'final is removed and privat on method is now protected'() {
+        setup:
+        Person person = Mock()
+
+        when:
+        def res = person.address.street
+
+        then:
+        1 * person.address >> new Person.Address('Yellow Brick Road')
+
+        expect:
+        res == 'Yellow Brick Road'
+    }
 
 }
