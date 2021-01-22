d846a6df72772356ddcc8ea6dbce6219ab5da63c
Joke de Buhr <joke@seiken.de>
2021-01-22 15:18:28 +0100

    added kotlin test

---
+++ examples/kotlin/src/test/groovy/io/github/joke/spockmockable/PersonTest.groovy
@@ -0,0 +1,61 @@
+package io.github.joke.spockmockable
+
+import org.spockframework.mock.MockUtil
+import spock.lang.Specification
+
+@Mockable([Person, Address])
+class PersonTest extends Specification {
+
+    def mockUtil = new MockUtil()
+
+    def 'final from class is removed'() {
+        setup:
+        Person person = Mock()
+
+        expect:
+        mockUtil.isMock person
+    }
+
+    def 'final from method is removed'() {
+        setup:
+        Person person = Mock()
+
+        when:
+        def res = person.firstName
+
+        then:
+        1 * person.firstName >> 'Dorothy'
+
+        expect:
+        res == 'Dorothy'
+    }
+
+    def 'privat on method is now protected'() {
+        setup:
+        Person person = Mock()
+
+        when:
+        def res = person.lastName
+
+        then:
+        1 * person.lastName >> 'Gale'
+
+        expect:
+        res == 'Gale'
+    }
+
+    def 'final is removed and privat on method is now protected'() {
+        setup:
+        Person person = Mock()
+
+        when:
+        def res = person.address.street
+
+        then:
+        1 * person.address >> new Address('Yellow Brick Road')
+
+        expect:
+        res == 'Yellow Brick Road'
+    }
+
+}
