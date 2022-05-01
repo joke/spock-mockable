ba14a7a79a60cd0ad1f08d915af18f0944767f09
Joke de Buhr <joke@xckk.de>
2022-05-01 13:17:37 +0200

    refactor: use gradle jvm-test-suite plugin
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- examples/spock13-groovy24/src/test/groovy/io/github/joke/spockmockable/tests/TestBase.groovy
+++
@@ -1,8 +0,0 @@
-package io.github.joke.spockmockable.tests
-
-import io.github.joke.spockmockable.Mockable;
-import spock.lang.Specification
-
-@Mockable([Person, Person.Address])
-abstract class TestBase extends Specification {
-}
