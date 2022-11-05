ed141511853b804f15b614002b72ba0c161617b7
Joke de Buhr <joke@xckk.de>
2022-11-05 18:36:41 +0100

    chore: basic unit testing setup
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- spock-mockable/src/main/java/io/github/joke/spockmockable/agent/RedefinitionListener.java
+++ spock-mockable/src/main/java/io/github/joke/spockmockable/agent/RedefinitionListener.java
@@ -21,7 +21,7 @@ public class RedefinitionListener extends AgentBuilder.RedefinitionStrategy.List
 
     @Override
     public Iterable<? extends List<Class<?>>> onError(final int index, final List<Class<?>> batch, final Throwable throwable, final List<Class<?>> types) {
-        log.warn("Could not transform classes '{}'", types);
+        log.warn("Could not transform classes: '{}'", types);
         return super.onError(index, batch, throwable, types);
     }
 
