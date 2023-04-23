bacda515ee12905ff544d0d812f03c1c2d691626
Joke de Buhr <joke@xckk.de>
2023-04-23 13:03:27 +0200

    ci: remove tool versions
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- examples/spock/src/main/java/io/github/joke/examples/Utility.java
+++ examples/spock/src/main/java/io/github/joke/examples/Utility.java
@@ -2,13 +2,15 @@ package io.github.joke.examples;
 
 public class Utility {
 
-    public static boolean state = false;
+    public static boolean state1 = false;
+    public static boolean state2 = false;
 
     public static String getName() {
         return Utility.class.getSimpleName();
     }
 
     public static void doUnsupported() {
+        state2 = true;
         throw new UnsupportedOperationException();
     }
 }
